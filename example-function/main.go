852a81291e1bfc45d99b8c87ea14fe8295f9c448
asigaru123 <two.626f6f6b@gmail.com>
2022-06-11 22:38:38 +0900

    feat: Add sam-example-function
    First commit of sam-example-function

---
+++ example-function/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Response struct {
+    RequestMethod  string `json:"RequestMethod"`
+    RequestBody    string `json:"RequestBody"`
+    PathParameter  string `json:"PathParameter"`
+    QueryParameter string `json:"QueryParameter"`
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	method := request.HTTPMethod
+	body := request.Body
+	pathParam := request.PathParameters["pathparam"]
+	queryParam := request.QueryStringParameters["queryparam"]
+
+	res := Response{
+		RequestMethod: method,
+		RequestBody: body,
+		PathParameter: pathParam,
+		QueryParameter: queryParam,
+	}
+	jsonBytes, _ := json.Marshal(res)
+	
+	return events.APIGatewayProxyResponse{
+		Body: string(jsonBytes),
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
